6538bb12c97dcba2d71677045f6ec34c49fa84cf
Spartacau5 <asa346@drexel.edu>
2021-11-18 19:32:17 -0500

    - Fixed Announcement Banner

--- src/components/layout/AnnouncementBanner/AnnouncementBanner.jsx
+++ src/components/layout/AnnouncementBanner/AnnouncementBanner.jsx
@@ -2,8 +2,7 @@ import { memo } from 'react';
 import clsx from 'clsx';
 import Container from '@components/layout/Container/Container';
 import classes from './AnnouncementBanner.module.css';
-import logoPng from '@images/polkastarter-logo.png';
-import logoWebp from '@images/polkastarter-logo.webp';
+
 
 const AnnouncementBanner = () => {
     return (
@@ -11,13 +10,9 @@ const AnnouncementBanner = () => {
             <Container>
                 <div className={classes.bannerInner}>
                     <h2 className={classes.bannerTitle}>
-                        $HYPEX IDO <span className="d-block">on</span>
+                        $HYPEX IDO <span className="d-block">in</span>
                     </h2>
                     <a href="/" target="_blank" rel="noopener noreferrer" className={classes.bannerLink}>
-                        <picture>
-                            <source srcSet={logoWebp} type="image/webp" />
-                            <img src={logoPng} alt="Polkastarter logo" />
-                        </picture>
                     </a>
                     <div className={classes.bannerEventDate}>Q1 2022</div>
                 </div>
