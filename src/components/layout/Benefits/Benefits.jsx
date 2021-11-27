bcbbb01af5ea5467b060e5ce7044afc9123b049c
jmwaffle <jackma0517@live.com>
2021-11-26 18:27:24 -0800

    period

--- src/components/layout/Benefits/Benefits.jsx
+++ src/components/layout/Benefits/Benefits.jsx
@@ -46,7 +46,7 @@ const Benefits = forwardRef((_, ref) => {
                                         <p>
                                         Users can stake NFTs to earn yields
                                         as well as deposit their NFTs for
-                                        collateral loans。
+                                        collateral loans.
                                         </p>
                                     </div>
                                 </FadeInSection>}
