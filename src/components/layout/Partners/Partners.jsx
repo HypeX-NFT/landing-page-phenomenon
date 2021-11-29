145c13bcfff9aabd28ca44486d23728b77e29d4b
jmwaffle <jackma0517@live.com>
2021-11-28 17:05:34 -0800

    partner color on hover

--- src/components/layout/Partners/Partners.jsx
+++ src/components/layout/Partners/Partners.jsx
@@ -2,165 +2,165 @@ import { forwardRef } from 'react';
 import clsx from 'clsx';
 import Container from '@components/layout/Container/Container';
 import classes from './Partners.module.css';
-import logo1Png from '@images/partner-logo-1.png';
-import logo1Webp from '@images/partner-logo-1.webp';
+import logo1Png from '@images/logo1.png';
+import logo1ColorPng from '@images/logo1-color.png';
 import logo2Png from '@images/partner-logo-2.png';
-import logo2Webp from '@images/partner-logo-2.webp';
-import logo3Png from '@images/partner-logo-3.png';
-import logo3Webp from '@images/partner-logo-3.webp';
-import logo4Png from '@images/partner-logo-4.png';
-import logo4Webp from '@images/partner-logo-4.webp';
-import logo5Png from '@images/partner-logo-5.png';
-import logo5Webp from '@images/partner-logo-5.webp';
+import logo2ColorPng from '@images/partner-logo-2.png';
+import logo3Png from '@images/logo3.png';
+import logo3ColorPng from '@images/logo3-color.png';
+import logo4Png from '@images/logo4.png';
+import logo4ColorPng from '@images/logo4-color.png';
+import logo5Png from '@images/logo5.png';
+import logo5ColorPng from '@images/logo5-color.png';
 import logo6Png from '@images/partner-logo-6.png';
-import logo6Webp from '@images/partner-logo-6.webp';
-import logo7Png from '@images/partner-logo-7.png';
-import logo7Webp from '@images/partner-logo-7.webp';
+import logo6ColorPng from '@images/partner-logo-6.png';
+import logo7Png from '@images/logo-7.png';
+import logo7ColorPng from '@images/logo-7-color.png';
 import logo8Png from '@images/partner-logo-8.png';
-import logo8Webp from '@images/partner-logo-8.webp';
+import logo8ColorPng from '@images/partner-logo-8.png';
 import logo9Png from '@images/partner-logo-9.png';
-import logo9Webp from '@images/partner-logo-9.webp';
-import logo10Png from '@images/partner-logo-10.png';
-import logo10Webp from '@images/partner-logo-10.webp';
-import logo11Png from '@images/partner-logo-11.png';
-import logo11Webp from '@images/partner-logo-11.webp';
-import logo12Png from '@images/partner-logo-12.png';
-import logo12Webp from '@images/partner-logo-12.webp';
+import logo9ColorPng from '@images/partner-logo-9.png';
+import logo10Png from '@images/logo-10.png';
+import logo10ColorPng from '@images/logo-10-color.png';
+import logo11Png from '@images/logo-11.png';
+import logo11ColorPng from '@images/logo-11-color.png';
+import logo12Png from '@images/logo-12.png';
+import logo12ColorPng from '@images/logo-12-color.png';
 import logo13Png from '@images/partner-logo-13.png';
-import logo13Webp from '@images/partner-logo-13.webp';
-import logo14Png from '@images/partner-logo-14.png';
-import logo14Webp from '@images/partner-logo-14.webp';
-import logo15Png from '@images/partner-logo-15.png';
-import logo15Webp from '@images/partner-logo-15.webp';
-import logo16Png from '@images/partner-logo-16.png';
-import logo16Webp from '@images/partner-logo-16.webp';
-import logo17Png from '@images/partner-logo-17.png';
-import logo17Webp from '@images/partner-logo-17.webp';
-import logo18Png from '@images/partner-logo-18.png';
-import logo18Webp from '@images/partner-logo-18.webp';
-import logo19Png from '@images/partner-logo-19.png';
-import logo19Webp from '@images/partner-logo-19.webp';
-import logo20Png from '@images/partner-logo-20.png';
-import logo20Webp from '@images/partner-logo-20.webp';
+import logo13ColorPng from '@images/partner-logo-13.png';
+import logo14Png from '@images/logo-14.png';
+import logo14ColorPng from '@images/logo-14-color.png';
+import logo15Png from '@images/logo-15.png';
+import logo15ColorPng from '@images/logo-15-color.png';
+import logo16Png from '@images/logo-16.png';
+import logo16ColorPng from '@images/logo-16-color.png';
+import logo17Png from '@images/logo-17.png';
+import logo17ColorPng from '@images/logo-17-color.png';
+import logo18Png from '@images/logo-18.png';
+import logo18ColorPng from '@images/logo-18-color.png';
+import logo19Png from '@images/logo-19.png';
+import logo19ColorPng from '@images/logo-19-color.png';
+import logo20Png from '@images/logo-20.png';
+import logo20ColorPng from '@images/logo-20-color.png';
 
 const partners = [
     {
         srcPng: logo1Png,
-        srcWebp: logo1Webp,
+        srcPngColor: logo1ColorPng,
         alt: 'Logo',
         link: 'https://polygon.technology/',
     },
     {
         srcPng: logo2Png,
-        srcWebp: logo2Webp,
+        srcPngColor: logo2ColorPng,
         alt: 'Logo',
         link: 'https://www.skyvisioncapital.com/',
     },
     {
         srcPng: logo3Png,
-        srcWebp: logo3Webp,
+        srcPngColor: logo3ColorPng,
         alt: 'Logo',
         link: 'https://polkastarter.com/',
     },
     {
         srcPng: logo4Png,
-        srcWebp: logo4Webp,
+        srcPngColor: logo4ColorPng,
         alt: 'Logo',
         link: 'https://www.incuba.capital/',
     },
     {
         srcPng: logo5Png,
-        srcWebp: logo5Webp,
+        srcPngColor: logo5ColorPng,
         alt: 'Logo',
         link: 'https://www.zonff.partners/#/home',
     },
     {
         srcPng: logo6Png,
-        srcWebp: logo6Webp,
+        srcPngColor: logo6ColorPng,
         alt: 'Logo',
         link: 'https://au21.capital/',
     },
     {
         srcPng: logo7Png,
-        srcWebp: logo7Webp,
+        srcPngColor: logo7ColorPng,
         alt: 'Logo',
         link: 'http://www.7xvc.com/',
     },
     {
         srcPng: logo8Png,
-        srcWebp: logo8Webp,
+        srcPngColor: logo8ColorPng,
         alt: 'Logo',
         link: 'http://ldcap.com/',
     },
     {
         srcPng: logo9Png,
-        srcWebp: logo9Webp,
+        srcPngColor: logo9ColorPng,
         alt: 'Logo',
         link: 'https://www.basics.vip/',
     },
     {
         srcPng: logo10Png,
-        srcWebp: logo10Webp,
+        srcPngColor: logo10ColorPng,
         alt: 'Logo',
         link: 'https://magnusdigitalassets.com/',
     },
     {
         srcPng: logo11Png,
-        srcWebp: logo11Webp,
+        srcPngColor: logo11ColorPng,
         alt: 'Logo',
         link: 'https://www.faculty.group/',
     },
     {
         srcPng: logo12Png,
-        srcWebp: logo12Webp,
+        srcPngColor: logo12ColorPng,
         alt: 'Logo',
         link: 'https://www.newtribe.capital/',
     },
     {
         srcPng: logo13Png,
-        srcWebp: logo13Webp,
+        srcPngColor: logo13ColorPng,
         alt: 'Logo',
         link: 'https://ghafcapital.ae/',
     },
     {
         srcPng: logo14Png,
-        srcWebp: logo14Webp,
+        srcPngColor: logo14ColorPng,
         alt: 'Logo',
         link: 'https://www.dcentralcon.com/',
     },
     {
         srcPng: logo15Png,
-        srcWebp: logo15Webp,
+        srcPngColor: logo15ColorPng,
         alt: 'Logo',
         link: 'https://formation.fi/',
     },
     {
         srcPng: logo16Png,
-        srcWebp: logo16Webp,
+        srcPngColor: logo16ColorPng,
         alt: 'Logo',
         link: 'https://www.complex.com/',
     },
     {
         srcPng: logo17Png,
-        srcWebp: logo17Webp,
+        srcPngColor: logo17ColorPng,
         alt: 'Logo',
         link: 'https://sneakercon.com/',
     },
     {
         srcPng: logo18Png,
-        srcWebp: logo18Webp,
+        srcPngColor: logo18ColorPng,
         alt: 'Logo',
         link: 'https://www.nicekicks.com/',
     },
     {
         srcPng: logo19Png,
-        srcWebp: logo19Webp,
+        srcPngColor: logo19ColorPng,
         alt: 'Logo',
         link: 'https://supbrousa.com/',
     },
     {
         srcPng: logo20Png,
-        srcWebp: logo20Webp,
+        srcPngColor: logo20ColorPng,
         alt: 'Logo',
         link: 'https://www.kinjaz.com/',
     },
@@ -178,12 +178,14 @@ const Partners = forwardRef((_, ref) => {
                 <ul className={classes.partnersList}>
                     {partners.map(partner => (
                         <li key={partner.srcPng} className={classes.partnersItem}>
-                            <div className={classes.partnersItemInner}>
-                                <picture>
-                                    <source srcSet={partner.srcWebp} type="image/webp" />
-                                    <img src={partner.srcPng} alt={partner.alt} onClick={()=>{
-                                        redirect(partner.link);
-                                    }}/>
+                            <div className={classes.partnersItemInner} onClick={()=>{
+                                redirect(partner.link);
+                            }}>
+                                <picture className={classes.black}>
+                                    <img src={partner.srcPng} alt={partner.alt} />
+                                </picture>
+                                <picture className={classes.color}>
+                                    <img src={partner.srcPngColor} alt={partner.alt} />
                                 </picture>
                             </div>
                         </li>
